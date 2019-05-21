a260838f1572312cb93927a0bfded7b4d37bbdcc
Danil Antonovich <sokoldaniil00@mail.ru>
2019-05-21 17:31:32 +0700

    Make board.h file

---
+++ src/board.h
@@ -0,0 +1,11 @@
+#ifndef _board_H_
+#define _board_H_
+#include "cmove.h"
+#include "figures.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void PrintBoard(char Board[8][8]);
+
+#endif
